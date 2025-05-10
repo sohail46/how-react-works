5a7b7c602208cb17bc69385ac041af0bd7b6bb06
Sohail Shaikh <sohail28195@gmail.com>
2025-05-10 16:19:42 +0530

    batch update + stale state learnings :zap:

--- src/App.js
+++ src/App.js
@@ -65,8 +65,36 @@ function TabContent({ item }) {
   const [showDetails, setShowDetails] = useState(true);
   const [likes, setLikes] = useState(0);
 
+  console.log("RENDER");
+
   function handleInc() {
-    setLikes(likes + 1);
+    setLikes((likes) => likes + 1); // always use callback as it avoids stale state
+  }
+
+  function handleTripleInc() {
+    // will not work, as state is stale
+    // setLikes(likes + 1);
+    // setLikes(likes + 1);
+    // setLikes(likes + 1);
+    // will work as state is not stale
+    // setLikes((likes) => likes + 1);
+    // setLikes((likes) => likes + 1);
+    // setLikes((likes) => likes + 1);
+
+    // will work as state is not stale
+    handleInc();
+    handleInc();
+    handleInc();
+  }
+
+  function handleUndoLater() {
+    setTimeout(handleUndo, 2000);
+  }
+
+  function handleUndo() {
+    setShowDetails(true);
+    setLikes(0);
+    console.log(likes); // state update is batched
   }
 
   return (
@@ -82,19 +110,19 @@ function TabContent({ item }) {
         <div className="hearts-counter">
           <span>{likes} ❤️</span>
           <button onClick={handleInc}>+</button>
-          <button>+++</button>
+          <button onClick={handleTripleInc}>+++</button>
         </div>
       </div>
 
       <div className="tab-undo">
-        <button>Undo</button>
-        <button>Undo in 2s</button>
+        <button onClick={handleUndo}>Undo</button>
+        <button onClick={handleUndoLater}>Undo in 2s</button>
       </div>
     </div>
   );
 }
 
-console.log(<DifferentContent test={23} />);
+// console.log(<DifferentContent test={23} />);
 
 function DifferentContent() {
   return (
